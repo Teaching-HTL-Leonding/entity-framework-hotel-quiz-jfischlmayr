385df049788438049451a4bd5ef8158f8d6d7e4f
jfischlmayr <jan.fischlmayr@gmx.net>
2020-12-09 20:49:16 +0100

    added HotelModel

---
+++ HotelExercise/HotelExercise/Hotel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HotelExercise
+{
+    class Hotel
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        private string Street { get; set; }
+        private int ZIPCode { get; set; }
+        [MaxLength(50)]
+        private string City { get; set; }
+        public string Address { get { return Street + ZIPCode + City; } }
+    }
+}
