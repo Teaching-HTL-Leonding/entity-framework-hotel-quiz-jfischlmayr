64ae23c143812a42a643f3f759f8899543b0774d
jfischlmayr <jan.fischlmayr@gmx.net>
2020-12-09 20:54:42 +0100

    added PriceModel

---
+++ HotelExercise/HotelExercise/Price.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelExercise
+{
+    class Price
+    {
+        public int Id { get; set; }
+        public int RoomTypeID { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public double PricePerNight { get; set; }
+    }
+}
