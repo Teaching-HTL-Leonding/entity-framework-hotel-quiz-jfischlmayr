28376512d8df9939fe58ab6a92b9cff18089ea0c
jfischlmayr <jan.fischlmayr@gmx.net>
2020-12-09 20:50:04 +0100

    added RoomTypes

---
+++ HotelExercise/HotelExercise/RoomType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelExercise
+{
+    class RoomType
+    {
+        public int Id { get; set; }
+        public Hotel Hotel { get; set; }
+        [MaxLength(50)]
+        public string Titel { get; set; }
+        [MaxLength(1000)]
+        public string Description { get; set; }
+        public int Size { get; set; }
+        public bool DisabilityAccesible { get; set; }
+        public int Amount { get; set; }
+    }
+}
