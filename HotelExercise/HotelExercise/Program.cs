f74b0467c088f2dceb1dbcdcbd89cd6b1237376f
jfischlmayr <jan.fischlmayr@gmx.net>
2020-12-09 19:43:25 +0100

    Added VS-Project

---
+++ HotelExercise/HotelExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HotelExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
